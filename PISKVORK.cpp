fc93d73cd1eceed03decd1fa3d73c1e69f226bfe
dhb <1084714805@qq.com>
2018-09-04 20:47:32 +0800

    newer interface style

--- PISKVORK.cpp
+++ PISKVORK.cpp
@@ -28,6 +28,8 @@
 #pragma comment(lib,"winmm.lib")
 #pragma comment(lib,"ole32.lib")
 
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 //-----------------------------------------------------------------
 /*
 USERC("Piskvork.rc");
